1710454db1bd350e6e0a5b6075cfee292f55fdbe
Luthfan <37529640+LuthfanM@users.noreply.github.com>
2022-09-05 20:42:51 +0700

    Update Configs.js

--- helpers/Configs.js
+++ helpers/Configs.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://maps.googleapis.com/maps/api";
-export const API_KEY = "AIzaSyDSKsr1WK1DcCmD49tsJ1nZMgKT8RJC9EE"
+export const API_KEY = "AIzaSyDSKsr1WK1DcCmD49tsJ1nZMgKT8RJC***"
